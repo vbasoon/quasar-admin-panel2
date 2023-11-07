93f66a3b324e54725ffd7ff059990f91213412a9
vbasoon <vbasoon@gmail.com>
2023-11-07 12:40:09 +0200

    full-width

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-page class="padding">
+  <q-page flex flex-center padding>
     <h1>Login Page</h1>
-    <q-card> </q-card>
+    <q-card class="full-width" style="max-width: 450px">
+      <q-card-section class="text-center"> Login </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
